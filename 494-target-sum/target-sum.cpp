b58adfbc60d8d6b001ceb1bdf9f87f929de87c3d
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-04-07 19:42:46 +0530

    Time: 16 ms (53.93%) | Memory: 25.8 MB (25.40%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int check(vector<int> &nums,int i,int target,int sum) {
+    int check(vector<int> &nums,int i,int target,int sum,vector<vector<int>> &dp,int offset) {
+        
          if(i<0) {
             if(sum==target) return 1;
             else return 0;
          }
+         if(dp[i][sum+offset]!=-1) return dp[i][sum+offset];
 
 
-         int neg=check(nums,i-1,target,sum+(-nums[i]));
+         int neg=check(nums,i-1,target,sum+(-nums[i]),dp,offset);
 
-         int pos=check(nums,i-1,target,sum+nums[i]);
+         int pos=check(nums,i-1,target,sum+nums[i],dp,offset);
 
-         return neg+pos;
+         return dp[i][sum+offset]=neg+pos;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-         return check(nums,nums.size()-1,target,0);
+        int sum=0;
+        for(auto it: nums) {
+            sum+=abs(it);
+        }
+        int offset=sum;
+
+        vector<vector<int>> dp(nums.size(),vector<int>(2*sum+1,-1));
+         return check(nums,nums.size()-1,target,0,dp,offset);
     }
-};
+}; 
