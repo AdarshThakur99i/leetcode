bdbfad9ee559ed1835f1dfaf49ef7eecfbccf4eb
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-04-09 17:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.50%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -4,11 +4,41 @@ public:
         int idx=0;
         vector<int> newarr;
         bool ist=false;
-           for(auto it:nums) {
-            if(it==target) {
-return true;
+           for(int i=0; i<nums.size()-1; i++) {
+           if(nums[i]>nums[i+1]) {idx=i+1; break; }
+           }
+
+           int l=idx;
+           int r=nums.size()-1;
+
+           while(l<=r) {
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return true;
+
+            else if(nums[mid]>target)  r=mid-1;
+
+            else {
+                l=mid+1;
             }
            }
+
+
+           l=0;
+           r=idx-1;
+            while(l<=r) {
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return true;
+
+            else if(nums[mid]>target)  r=mid-1;
+
+            else {
+                l=mid+1;
+            }
+           }
+
+
 
            return false;
            
