38874b928fc93d6d20779ddb49c7b397c70835a7
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-04-09 17:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.50%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int idx=0;
+        vector<int> newarr;
+        bool ist=false;
+           for(auto it:nums) {
+            if(it==target) {
+return true;
+            }
+           }
+
+           return false;
+           
+    }
+};
