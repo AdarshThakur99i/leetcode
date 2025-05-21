8b65df35bfcd597064c3f37ca1c80bc85cd4ddf5
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-05-21 09:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (6.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-          vector<vector<int>> used(matrix.size(),vector<int>(matrix[0].size(),0));
-          for(int i=0; i<matrix.size(); i++) {
-            for(int j=0; j<matrix[0].size(); j++) {
-                if(matrix[i][j]==0 && used[i][j]==0) {
-                    used[i][j]=1;
-                   
-                    int p=0;
-                    while(p<matrix[0].size()) {
-                        if(matrix[i][p]!=0) {
-                        matrix[i][p]=0;
-                        used[i][p]=1;}
-                        p++;
-                    }
-                   int k=0;
-                    
-                    while(k<matrix.size()) {
-                        if(matrix[k][j]!=0) {
-                        matrix[k][j]=0;
-                        used[k][j]=1; }
-                        k++;
-                    }
+         vector<vector<int>> vis=matrix;
 
+       for(int i=0; i<matrix.size(); i++) {
+        for(int j=0; j<matrix[0].size(); j++) {
+            if(vis[i][j]==0) {
+           for(int k=0; k<matrix.size(); k++) {
+            matrix[k][j]=0;
+           }
 
-                }
+           for(int k=0; k<matrix[0].size(); k++) {
+            matrix[i][k]=0;
+           }
+
             }
-          }
+        }
+       }
+        
      
         }
     
