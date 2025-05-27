19bed306a07e7d0e96db6b383b155b08d705022c
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-05-27 11:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (96.45%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int l=0;
+        int n=citations.size();
+        int r=citations.size()-1;
+        while(l<=r) {
+           int mid=l+(r-l)/2;
+           
+           if((n-mid)==citations[mid]) {
+               return n-mid;
+           }
+           else if((n-mid)  < citations[mid]) {
+            r=mid-1;
+
+           }
+           else {
+            l=mid+1;
+           }
+        }
+
+return n-l;
+        }    
+};
