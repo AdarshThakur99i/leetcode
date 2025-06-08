cf56911a5608d42ef291e3830a7d55b17220ce23
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-06-08 17:34:18 +0530

    Time: 4 ms (28.88%) | Memory: 19.6 MB (28.47%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
 
-    int check(vector<int> &prices,int i,int buy,vector<vector<int>> &dp) {
-        
-        if(i>=prices.size()) return 0;
+    int maxProfit(vector<int>& prices) {
+      vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+    dp[prices.size()][0]=dp[prices.size()][1]=0;
 
-        if(dp[i][buy]!=-1) return dp[i][buy];
+      for(int i=prices.size()-1; i>=0; i--) {
+        for(int j=0; j<2; j++) {
 int buy_1=0;
 int not_buy=0;
 int profit=0;
 
-     if(!buy) {
+     if(!j) {
         
-     buy_1=-prices[i]+check(prices,i+1,1,dp);
-     not_buy=check(prices,i+1,0,dp);
+     buy_1=-prices[i]+dp[i+1][1];
+     not_buy=dp[i+1][0];
      profit=max(buy_1,not_buy);
      }
      int sell=0;
      int not_sell=0;
-     if(buy) {
+     if(j) {
         
-         sell= prices[i]+check(prices,i+1,0,dp);
-         not_sell=check(prices,i+1,1,dp);
+         sell= prices[i]+dp[i+1][0];
+         not_sell=dp[i+1][1];
          profit=max(sell,not_sell);
      }
-
-     return dp[i][buy]=profit;
-     
-
-    } 
-    int maxProfit(vector<int>& prices) {
-      vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return check(prices,0,0,dp);
+dp[i][j]=profit;
+        }
+      }
+        return dp[0][0];
          
 
 
