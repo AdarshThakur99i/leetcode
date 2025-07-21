4f022c0bf0686e16a35ebde52c0804af950b05b9
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-07-21 22:52:22 +0530

    Time: 186 ms (53.69%) | Memory: 12.8 MB (44.13%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-
-  int primes(int n) {
-    vector<int> temp(n,0); 
-temp[0]=1;
-temp[1]=1;
-
-    for(int i=2; i<sqrt(n); i++) {
-    
-        for(int j=i*i; j<n; j+=i) {
-            temp[j]=1;
-
-        
-    }}
-
-   int count=0;
-   for(int i=0; i<temp.size(); i++) {
-    if(temp[i]==0) count++;
-
-
-
-   }
-   return count;
-  }
     int countPrimes(int n) {
-        if(n<=1) return 0;
-        
+        if (n <= 2) return 0;
 
-        return primes(n);
-        
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i < n; ++i) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return count(isPrime.begin(), isPrime.end(), true);
     }
 };
