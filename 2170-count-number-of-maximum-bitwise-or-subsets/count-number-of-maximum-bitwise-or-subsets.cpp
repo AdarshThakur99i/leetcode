854cc134a77619eb2579341cd9328bf9353ebde3
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-07-28 12:09:41 +0530

    Time: 46 ms (41.18%) | Memory: 29 MB (18.84%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+      unordered_map<string,int> mp;
     int check(vector<int> &nums,int &maxi,int i,int currOrr) {
     
              if(i==nums.size()) {
@@ -16,15 +17,20 @@ public:
                 }
                
              }
+             string key=to_string(maxi)+','+to_string(i)+','+to_string(currOrr);
+             if(mp.count(key)) {
+                return mp[key];
+             }
                   
            int take=  check(nums,maxi,i+1,currOrr | nums[i]);
            int not_take=  check(nums,maxi,i+1,currOrr);
-         return take+not_take;
+         return mp[key]=take+not_take;
 
     }
     int countMaxOrSubsets(vector<int>& nums) {
       int maxi=INT_MIN;
       int cnt=0;
+      
      int res= check(nums,maxi,0,0);
       return res;
 
