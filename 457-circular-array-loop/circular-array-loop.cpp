2c8028ccd289c431a7dac4ebe2058c528bc9703b
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-07-31 17:17:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (100.00%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.cpp
+++ 457-circular-array-loop/circular-array-loop.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
+   int getIdx(int i,vector<int> &nums) {
+    int n=nums.size();
+    return (((i+nums[i])%n)+n)%n;
+   }
     bool circularArrayLoop(vector<int>& nums) {
-        
-         int i=0;
-         int n=nums.size();
-         for(int j=0; j<nums.size(); j++) {
-            i=j;
-             vector<int> vis(nums.size(),0);
-             bool isForward= (nums[i]>0);
-         while(true) {
-int prev=i;
-int next=i+nums[i];
- next=((next%n)+n)%n;
-           
-           if(next==prev || (nums[next]>0) != isForward ) {
-            break;
-           }
-            if(vis[i]==1) {
+          int i=0;
+          int n=nums.size();
+          for(int i=0; i<nums.size(); i++) {
+            if(nums[i]==0) continue;
+             bool isForward=(nums[i]>0);
+int slow=i;
+int fast=i;
+            while(true) {
+                slow=getIdx(slow,nums);
+               fast=getIdx(fast,nums);
+               if((nums[fast]>0) != isForward || nums[fast]==0) {
+                break;
+               }
+               fast=getIdx(fast,nums);
+ if((nums[fast]>0) != isForward || nums[fast]==0) {
+                break;
+               }
+               if(slow==fast){
+                if(getIdx(slow,nums)==slow) break;
                 return true;
+               }
+               if((nums[slow]>0) != isForward || nums[slow]==0) break;
             }
-            vis[i]=1;
-            i=next;
-         }}
 
-         return false;
+            int index=i;
+            while((nums[index]>0)==isForward && nums[index]!=0) {
+                int newid=getIdx(index,nums);
+                nums[index]=0;
+                index=newid;
+            }
+          }
+          return false;
     }
 };
