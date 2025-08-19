552f3bda538d16a90309b479fc876a05bac8e541
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-08-20 00:47:17 +0530

    Time: 4 ms (27.82%) | Memory: 111.3 MB (74.50%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int i=0;
+        int j=0;
+        long long cnt=0;
+unordered_map<int,int> mp;
+        while(j<nums.size()) {
+if(nums[j]==0) {
+    i=j;
+    while(j<nums.size() && nums[j]==0) {
+   
+    cnt+=(j-i+1);
+   
+        j++;
+    }
+}
+j++;
+        }
+
+      
+        return cnt;
+    }
+};
+
+//a b c 
+//abc acb bac bca cab cba
