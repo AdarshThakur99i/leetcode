e36fd0f39d1e0ec85297ca6c0dbbd890d26648de
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-08-23 11:26:30 +0530

    Time: 1 ms (38.51%) | Memory: 8.9 MB (42.34%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,26 +2,45 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
           int cntone=0;
-          string mini="1e9";
+          string mini="";
           int minlen=INT_MAX;
-             for(int i=0; i<s.size(); i++) {
-                    cntone=0;
-                for(int j=i; j<s.size(); j++) {
-                    if(s[j]=='1') {
-                        cntone++;
-                    }
-                  if(cntone==k && (j-i+1)<minlen) {
-                    string temp=s.substr(i,j-i+1);
+         int i=0;
+         int j=0;
+         while(j<s.size()) {
+            if(s[j]=='1') cntone++;
+  
+            while(cntone>=k) {
+                   if(cntone==k && j-i+1<minlen) {
+                 string temp=s.substr(i,j-i+1);
                     mini=temp;
                     minlen=(j-i+1);
-                  }
-                  else if(cntone==k && (j-i+1)==minlen) {
-                     string temp=s.substr(i,j-i+1);
+            }
+            else if(cntone==k && j-i+1 ==minlen) {
+                 string temp=s.substr(i,j-i+1);
                     mini=min(mini,temp);
-                  }
-                  if(cntone>k) break;
-                }
-             }
-             return (mini=="1e9")?"":mini;
+            }
+                if(s[i]=='1') cntone--;
+                i++;
+            }
+
+         
+            // if(j==s.size()-1) {
+            //     while(cntone==k) {
+            //           if(cntone==k && j-i+1<minlen) {
+            //      string temp=s.substr(i,j-i+1);
+            //         mini=temp;
+            //         minlen=(j-i+1);
+            // }
+            // else if(cntone==k && j-i+1 ==minlen) {
+            //      string temp=s.substr(i,j-i+1);
+            //         mini=min(mini,temp);
+            // }
+            //         if(s[i]=='1') cntone--;
+            //         i++;
+            //     }
+            // }
+            j++;
+         }
+             return mini;
     }
 };
