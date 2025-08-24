0f657162cfc7bfcfa488e598a01621efabccae03
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-08-24 18:18:22 +0530

    Time: 101 ms (5.70%) | Memory: 119.4 MB (5.08%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(unordered_map<int,vector<int>>& adj,int i,vector<int> &vis) {
+  vis[i]=1;
+        for(auto it: adj[i])  {
+            if(!vis[it]) {
+                dfs(adj,it,vis);
+            }
+        }
+    }
+    int findChampion(int n, vector<vector<int>>& edges) {
+        unordered_map<int,vector<int>> adj;
+        for(auto it: edges ) {
+            adj[it[0]].push_back(it[1]);
+        }
+           
+int cnt=0;   int ans=-1;
+           for(int i=0; i<n; i++) {
+         vector<int> vis(n,0); 
+            dfs(adj,i,vis);
+         
+            bool ok=true;
+              for(int j=0; j<n; j++) {
+
+                if(vis[j]!=1) {
+                    ok=false;
+                    break;
+                }
+              }
+              if(ok==true) {
+                if(cnt>0) return -1;
+                ans=i;
+                cnt++;
+              }
+
+           }
+        return ans;
+
+    }
+};
