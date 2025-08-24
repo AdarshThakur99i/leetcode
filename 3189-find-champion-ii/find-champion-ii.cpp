71f9a4b8108702d45872620d650bf9288d744a7a
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-08-24 18:22:21 +0530

    Time: 3 ms (67.88%) | Memory: 94 MB (63.73%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -1,40 +1,54 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>>& adj,int i,vector<int> &vis) {
-  vis[i]=1;
-        for(auto it: adj[i])  {
-            if(!vis[it]) {
-                dfs(adj,it,vis);
-            }
+//     void dfs(unordered_map<int,vector<int>>& adj,int i,vector<int> &vis) {
+//   vis[i]=1;
+//         for(auto it: adj[i])  {
+//             if(!vis[it]) {
+//                 dfs(adj,it,vis);
+//             }
+//         }
+//     }
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> indegree(n,0);
+
+    for(int i=0; i<edges.size(); i++) {
+        indegree[edges[i][1]]++;
+    }
+int cnt=0;
+int ans=-1;
+    for(int i=0; i<indegree.size(); i++) {
+        if(indegree[i]==0) {
+            if(cnt>0) return -1;
+            ans=i;
+            cnt++;
         }
     }
-    int findChampion(int n, vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>> adj;
-        for(auto it: edges ) {
-            adj[it[0]].push_back(it[1]);
-        }
+//         unordered_map<int,vector<int>> adj;
+//         for(auto it: edges ) {
+//             adj[it[0]].push_back(it[1]);
+//         }
            
-int cnt=0;   int ans=-1;
-           for(int i=0; i<n; i++) {
-         vector<int> vis(n,0); 
-            dfs(adj,i,vis);
+// int cnt=0;   int ans=-1;
+//            for(int i=0; i<n; i++) {
+//          vector<int> vis(n,0); 
+//             dfs(adj,i,vis);
          
-            bool ok=true;
-              for(int j=0; j<n; j++) {
+//             bool ok=true;
+//               for(int j=0; j<n; j++) {
 
-                if(vis[j]!=1) {
-                    ok=false;
-                    break;
-                }
-              }
-              if(ok==true) {
-                if(cnt>0) return -1;
-                ans=i;
-                cnt++;
-              }
+//                 if(vis[j]!=1) {
+//                     ok=false;
+//                     break;
+//                 }
+//               }
+//               if(ok==true) {
+//                 if(cnt>0) return -1;
+//                 ans=i;
+//                 cnt++;
+//               }
 
-           }
-        return ans;
+//            }
+         return ans;
 
     }
 };
