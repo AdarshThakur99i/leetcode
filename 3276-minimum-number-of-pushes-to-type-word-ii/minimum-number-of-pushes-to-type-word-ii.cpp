b418a5737fb7fe84f9898a2da8c2a2f14ddf36ea
Adarsh Thakur <141508060+AdarshThakur398@users.noreply.github.com>
2025-09-01 22:45:53 +0530

    Time: 33 ms (31.96%) | Memory: 26.9 MB (50.78%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,35 +6,30 @@ public:
            for(int i=0; i<word.size(); i++) {
             mp[word[i]]++;
            }
-           int sum=0;
+     
            for(auto it: mp) {
             q.push(it.second);
-            sum+=it.second;
+    
            }
            int curr=0;
-           if(mp.size()<=8) return sum;
-             int temp=3;
-           int extra=mp.size()%8; int val=1;
-           while(temp>0) {
+
+          int cnt=0;
+        int val=1;
            
-            for(int i=0; i<8; i++) {
-           if (!q.empty()) {
+           
+        
+           while (!q.empty()) {
               curr+=(val)*q.top();
               q.pop();
+              cnt++;
+             if(cnt%8==0)val++;
 
-
            }
-           }
-           val++;
-           temp--;
-           }
-           if(!q.empty() && extra>0) {
-            while(!q.empty() && extra>0) {
-                curr += (4*q.top());
-                q.pop();
-                extra--;
-            }
-           }
+           
+          
+          
+           
+       
        
            return curr;
     }
