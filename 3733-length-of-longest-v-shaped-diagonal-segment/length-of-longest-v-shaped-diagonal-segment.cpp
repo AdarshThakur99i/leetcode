0145b589a51b1f4fb93a517200d4b7008eddc771
Adarsh Thakur <141508060+AdarshThakur99i@users.noreply.github.com>
2025-11-03 23:41:21 +0530

    Time: 1656 ms (30.72%) | Memory: 95.2 MB (81.85%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-  vector<vector<int>> dir={{1,1},{1,-1},{-1,-1},{-1,1}};
-    int solve(int i,int j,int d,bool canTurn,int val,vector<vector<int>>& grid) {
+vector<vector<int>> dir={{1,1},{1,-1},{-1,-1},{-1,1}};
+    int check(vector<vector<int>>& grid,int i,int j,bool canTurn ,int d,int val ) {
               
-              int newi=i+dir[d][0];
+              int newr=i+dir[d][0];
               int newc=j+dir[d][1];
               int keepmoving=0;
               int move=0;
               int res=0;
-              if(newi<grid.size() && newi >=0 && newc < grid[0].size() && newc>=0 && grid[newi][newc]==val)  {
-                        keepmoving=1+solve(newi,newc,d,canTurn,(val==2)?0:2,grid);
-                       if(canTurn==true) {
-                        move=max(keepmoving,1+solve(newi,newc,(d+1)%4,false,(val==2)?0:2,grid));
-                       }
-                       res=max(move,keepmoving);
-              }
-              return res;
+            if(newr>=0 && newr<grid.size() && newc >=0 && newc < grid[0].size() && grid[newr][newc] == val) {
+                            keepmoving=1+check(grid,newr,newc,canTurn,d,(val==2)?0:2);
+                           if(canTurn==true) {
+                                    move =max(keepmoving,1+check(grid,newr,newc,false,(d+1)%4,(val==2)?0:2));
+                           } 
+                           res=max(move,keepmoving); 
+            }
+             
+              
+        return res;
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int res=0;
+          int res=0;
           for(int i=0; i<grid.size(); i++) {
             for(int j=0; j<grid[0].size(); j++) {
                 if(grid[i][j]==1) {
-                     for(int d=0; d<=3; d++) {
-                       res=max(res,1+solve(i,j,d,true,2,grid));
-                     }
+                for(int d=0; d<=3; d++) {
+res=max(res,1+check(grid,i,j,true,d,2));
                 }
-               
-            }
+                      
+
+            }}
           }
+
           return res;
     }
 };
