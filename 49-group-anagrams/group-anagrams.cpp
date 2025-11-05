65444d54b1bcc3be9391493d3ae827384f7b34e4
Adarsh Thakur <141508060+AdarshThakur99i@users.noreply.github.com>
2025-11-05 11:42:19 +0530

    Time: 26 ms (26.71%) | Memory: 27.3 MB (25.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> original;
-        for(int i=0; i<strs.size(); i++) {
-            original.push_back(strs[i]);
-        }
-          for(auto &it:strs) {
-            sort(begin(it),end(it));
-          }
-          vector<bool> used(strs.size(),false);
-vector<vector<string>> ans;
-for(int i=0; i<strs.size(); i++) {
-    if(used[i]) continue;
+        unordered_map<string,vector<int>> mp;
+        vector<string> x=strs;
+            for(int i=0; i<strs.size(); i++) {
+                sort(begin(x[i]),end(x[i]));
+            }
+         for(int i=0; i<x.size(); i++) {
+            mp[x[i]].push_back(i);
+         }
+         int j=0;
+     vector<vector<string>> ans;
+     for(auto it: mp) {
 
-    vector<string> temp;
-
-    for(int j=i; j<strs.size(); j++) {
-        if(!used[j] && strs[i]==strs[j]) {
-            used[j]=true;
-            temp.push_back(original[j]);
-             
-        }
-    }
-    ans.push_back(temp);
-}
-
-return ans;
-
+        string temp=it.first;
+        vector<string> groups;
+       for(auto it: it.second) {
+        groups.push_back(strs[it]);
+       }
+       ans.push_back(groups);
+       
+     }
+     return ans;
     }
 };
